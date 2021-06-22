ed0345c0ff08f4eaeaa9adb3e76ab98486aaba6e
OrestesX <orestes@matematica.xyz>
2021-06-21 23:58:33 -0300

    final aula 2

---
+++ main.js
@@ -0,0 +1,31 @@
+/* abre e fecha o menu quando clicar nos icones*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* abre e fecha o menu quando clicar nos icones*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da pagina no scroll*/
+
+const header = document.querySelector('header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
